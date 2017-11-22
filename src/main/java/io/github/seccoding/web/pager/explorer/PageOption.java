82226153a6ca62c96143cee9cbd7fa3c942ad9af
Min Chang Jang <mcjang1116@gmail.com>
2017-11-22 23:32:15 +0900

    PageOption 추가. PageExplorer 에 데이터를 담을 수 있는 List 추가.
    MVC 예제 추가
    
    --
    
    주석 위치 변경

--- src/main/java/io/github/seccoding/web/pager/explorer/PageOption.java
+++ src/main/java/io/github/seccoding/web/pager/explorer/PageOption.java
@@ -2,40 +2,25 @@ package io.github.seccoding.web.pager.explorer;
 
 public class PageOption {
 
-    /**
-     * 서버에게 전달될 Form 의 아이디를 작성한다.<br/>
-     * 기본값 : searchForm
-     */
+
     private String formId = "searchForm";
 
-    /**
-     * Page 번호를 전달할 Parameter Name<br/>
-     * 기본값 : pageNo
-     */
     private String link = "pageNo";
 
-    /**
-     * Page 번호를 보여줄 패턴 @(at)가 페이지 번호로 치환된다. [@]로 작성할 경우 [1] [2] [3] 처럼 보여진다.<br/>
-     * 기본값 : [@]
-     */
     private String pageFormat = "[@]";
 
-    /**
-     * 이전 페이지 그룹으로 가는 버튼의 이름을 작성한다.<br/>
-     * 기본값 : Prev
-     */
     private String prev = "Prev";
 
-    /**
-     * 다음 페이지 그룹으로 가는 버튼의 이름을 작성한다.<br/>
-     * 기본값 : Next
-     */
     private String next = "Next";
 
     public String formId() {
         return formId;
     }
 
+    /**
+     * 서버에게 전달될 Form 의 아이디를 작성한다.<br/>
+     * 기본값 : searchForm
+     */
     public PageOption formId(String formId) {
         this.formId = formId;
         return this;
@@ -45,6 +30,10 @@ public class PageOption {
         return link;
     }
 
+    /**
+     * Page 번호를 전달할 Parameter Name<br/>
+     * 기본값 : pageNo
+     */
     public PageOption link(String link) {
         this.link = link;
         return this;
@@ -54,6 +43,10 @@ public class PageOption {
         return pageFormat;
     }
 
+    /**
+     * Page 번호를 보여줄 패턴 @(at)가 페이지 번호로 치환된다. [@]로 작성할 경우 [1] [2] [3] 처럼 보여진다.<br/>
+     * 기본값 : [@]
+     */
     public PageOption pageFormat(String pageFormat) {
         this.pageFormat = pageFormat;
         return this;
@@ -63,6 +56,10 @@ public class PageOption {
         return prev;
     }
 
+    /**
+     * 이전 페이지 그룹으로 가는 버튼의 이름을 작성한다.<br/>
+     * 기본값 : Prev
+     */
     public PageOption prev(String prev) {
         this.prev = prev;
         return this;
@@ -72,6 +69,10 @@ public class PageOption {
         return next;
     }
 
+    /**
+     * 다음 페이지 그룹으로 가는 버튼의 이름을 작성한다.<br/>
+     * 기본값 : Next
+     */
     public PageOption next(String next) {
         this.next = next;
         return this;
